b53e08f681c9dca5f1480db169de5d1af9cf0588
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2023-04-08 11:53:15 -0700

    Added Functions Inside API.swift to generate base URL to access data from PokeAPI - Added Function "getURLForPokemonList(offset: Int, limit: Int)" a URL used to obtain the list off all available Pokémon - Added Function "getURLForPokemonDetails(id: Int)"  a URL used to access the details about a specific Pokemon determined by its id - Added Function "getURLForPokemonSpecies(name: String)" a URL used to access more detailed information about a specific Pokemon in relation to its "Species"

--- Pokedex/Networking/API.swift
+++ Pokedex/Networking/API.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct API {
+    static let baseURLString = "https://pokeapi.co/api/v2/"
+    
+    static func getURLForPokemonList(offset: Int, limit: Int) -> String {
+        return "\(baseURLString)pokemon-species?offset=\(offset))&limit=\(limit)"
+    }
+    
+    static func getURLForPokemonDetails(id: Int) -> String {
+        return "\(baseURLString)pokemon/\(id)/"
+    }
+    
+    static func getURLForPokemonSpecies(name: String) -> String {
+        return "\(baseURLString)pokemon-species/\(name)/"
+    }
+}
