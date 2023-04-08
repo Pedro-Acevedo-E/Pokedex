37f5053584620a4dc512809dc632aa05c9796da8
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2023-04-08 11:20:30 -0700

    Initial commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Pedro Acevedo on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
